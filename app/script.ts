a73b7115c16c4abc93dbb18a0663e7fc206f567e
Jeremy Berteloot <berteloot.jeremy@gmail.com>
2023-01-27 12:22:02 +0100

    update target channel

--- app/script.ts
+++ app/script.ts
@@ -2,19 +2,21 @@ import { Types } from "ably/promises";
 import * as Ably from "ably/promises";
 
 (async () => {
+  console.log("Oh hai! 🖤");
 
-    console.log("Oh hai! 🖤");
+  const optionalClientId = "optionalClientId"; // When not provided in authUrl, a default will be used.
+  const ably = new Ably.Realtime.Promise({
+    authUrl: `/api/ably-token-request?clientId=${optionalClientId}`,
+  });
+  const channel = ably.channels.get("getting-started");
 
-    const optionalClientId = "optionalClientId"; // When not provided in authUrl, a default will be used.
-    const ably = new Ably.Realtime.Promise({ authUrl: `/api/ably-token-request?clientId=${optionalClientId}` });
-    const channel = ably.channels.get("some-channel-name");
+  await channel.subscribe((msg: Types.Message) => {
+    console.log("Ably message received", msg);
+    document.getElementById("response").innerHTML +=
+      "<br />" + JSON.stringify(msg);
+  });
 
-    await channel.subscribe((msg: Types.Message) => {
-        console.log("Ably message received", msg);
-        document.getElementById("response").innerHTML += "<br />" + JSON.stringify(msg);
-    });
-
-    channel.publish("hello-world-message", { message: "Hello world!" });
+  channel.publish("hello-world-message", { message: "Hello world!" });
 })();
 
-export { };
+export {};
